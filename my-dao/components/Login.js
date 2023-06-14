34756e7f7f19c56c6df0b80d41abaae2ac4773bc
Bentax <bentaxrus@mail.ru>
2023-06-14 18:20:13 +0300

    Last

---
+++ my-dao/components/Login.js
@@ -0,0 +1,10 @@
+import { useMetamask } from "@thirdweb-dev/react";
+const Login = () => {
+  const connectWithMetamask = useMetamask();
+  return (
+    <div>
+      <button onClick={connectWithMetamask}>Sign in using MetaMask</button>
+    </div>
+  );
+};
+export default Login;
